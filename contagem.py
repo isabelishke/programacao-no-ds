b5334e463377289d481a39364187834c046706e4
isabelishke <isabeli.hanke@escola.pr.gov.br>
2025-06-05 11:59:59 -0300

    Create contagem.py

---
+++ contagem.py
@@ -0,0 +1,5 @@
+# Solicita ao usuário que digite uma frase
+frase = input("Digite uma frase: ")
+
+# Conta e exibe o número de caracteres na frase
+print(f"\nA frase contém {len(frase)} caracteres.")
